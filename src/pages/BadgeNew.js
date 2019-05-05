5106a5f596e22f6a7b179a65aa3c48efa643192f
Edward Fernández <edanfesi@gmail.com>
2019-05-04 22:57:52 -0500

    Add the pages folder and the Badge New page
    I separated the components and the pages creating a new folder for only the pages and it style. In the process I created the Badge New page where the user will be able to add new badges.

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import "./styles/BadgeNew.css";
+import Navbar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+
+class BadgeNew extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Edward"
+                lastName="Fernández"
+                avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                jobTitle="Frontend Developer"
+                twitter="edanfesi"
+              />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
