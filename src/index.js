474aeb97717c93565b7528c387aac36b7b6c1685
Edward Fernández <edanfesi@gmail.com>
2019-05-16 21:34:47 -0500

    Add the Bootstrap style to the site
    For the first release, I will use the Bootstrap style. In the future, I will use CSS Grid.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import BadgeNew from "./pages/BadgeNew";
+import App from "./components/App";
 
 const container = document.getElementById("root");
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<App />, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
