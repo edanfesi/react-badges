e6bb8d3baa4058bfa1e1792d3d25266acaa9aac6
Edward Fernández <edanfesi@gmail.com>
2019-05-16 21:31:41 -0500

    Create a component for the Navbar and a component for the Layout
    To make a clean structure for this project I decided to separate the layout and create a component for it because the Navbar will not change in any circumstance. As well I decided to create a component that will be used for the layout.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./styles/Navbar.css";
+
+import { Link } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 class Navbar extends Component {
@@ -7,11 +9,11 @@ class Navbar extends Component {
     return (
       <div className="Navbar">
         <div className="container-fluid">
-          <a href="/" className="Navbar__brand">
+          <Link to="/" className="Navbar__brand">
             <img src={logo} alt="Logo" className="Navbar__brand-logo" />
             <span className="font-weight-light">Platzi</span>
             <span className="font-weight-bold">Conf</span>
-          </a>
+          </Link>
         </div>
       </div>
     );
